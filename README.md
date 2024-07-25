d055dd6b6d060f31874c469ca226dd082695d9b4
6millions <kittichai.s@ic-connection.com>
2024-07-25 12:26:06 +0700

    finish practice today

--- README.md
+++ README.md
@@ -2,15 +2,16 @@
 Practice Array In TypeScript
 
 ## Overview
-Play with map function in array
+Play with map function in array - Specialize in Union Type
 
 ## Installation
 1. Clone the repository
 2. Install dependencies: npm install
 
 ## Problem Explanation
-
-
+1. Define object type
+2. Include the number and string in object 
+3. pass value to combine both number and string to test union type
 
 ## License
 This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,89 @@ app.use(bodyParser.json());
 
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript with Express!');
+
+    const msg = executeArray();
+
+    //return msg;
+    res.send(msg);
+
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+interface Person {
+    name: string;
+    age: number;
+    travelPlace: string[];
+    expList: number[];
+
+}
+
+function executeArray(): string {
+
+    console.log("[executeArray]");
+
+    // person 1
+    const Jack: Person = {
+        name: "Jack",
+        age: 16,
+        travelPlace: ['Thailand', 'Singapore', 'Hongkok'],
+        expList: [11, 19, 20]
+
+    }
+
+    // person 2
+    const John: Person = {
+        name: "John",
+        age: 18,
+        travelPlace: ['Italy', 'UK', 'Scotland', 'Ireland'],
+        expList: [99]
+    }
+
+    combineArray(Jack.expList, John.expList);
+    combineArray(Jack.travelPlace, John.travelPlace);
+
+    return "test";
+}
+
+function combineArray(input1: number[] | string[], input2: number[] | string[]) {
+    console.log("[combineArray]");
+    console.log(input1, input2);
+    let result;
+
+    if (
+        Array.isArray(input1) && Array.isArray(input2) &&
+        (input1.every(item => typeof item === 'number')) &&
+        (input2.every(item => typeof item === 'number'))
+    ) {
+
+        // sum all exp
+        console.log("sum number");
+        const combineNum = [...input1 as number[], ...input2 as number[]];
+
+        result = combineNum.reduce((prev, current) => prev + current, 0);
+
+        console.log(result);
+
+
+    }
+    else if (
+        Array.isArray(input1) && Array.isArray(input2) &&
+        (input1.every(item => typeof item === 'string')) &&
+        (input2.every(item => typeof item === 'string'))
+    ) {
+
+
+        const combineString = [...input1 as string[], ...input2 as string[]];
+        // sum all exp
+        console.log("combine text");
+        result = combineString.join(',') + ',' + input2.join(',');
+
+        console.log(result);
+
+    }
+    return result;
+}
